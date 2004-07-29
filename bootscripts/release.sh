bcb59552b9bf83d4fe77e503804cbd234ea499f1
dj <dj@af4574ff-66df-0310-9fd7-8a98e5e911e0>
2004-07-29 07:09:03 +0000

    added shell script to make tarball
    git-svn-id: svn://svn.linuxfromscratch.org/BLFS/trunk@2515 af4574ff-66df-0310-9fd7-8a98e5e911e0

---
+++ bootscripts/release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Creates a dated (or released tarball)
+
+if [ ! $UID = "0" ]
+then
+	echo "This script must be run as root!!!"
+	exit 1
+else
+
+if [ -n "$1" ]
+then
+	RELEASE=$1
+else
+	RELEASE=`date +%Y%m%d`
+fi
+
+ORPWD=$PWD
+RTPWD=$ORPWD/../blfs-bootscripts-$RELEASE
+
+cp -a $ORPWD $RTPWD &&
+rm -f $RTPWD/{README.release,release.sh} &&
+chown root:root -R $RTPWD &&
+chmod 644 -R $RTPWD &&
+cd .. &&
+chmod 766 blfs-bootscripts-$RELEASE &&
+chmod 755 blfs-bootscripts-$RELEASE/{,blfs{,/init.d,/sysconfig{,/network-devices{,/services}}}} &&
+tar -cf $ORPWD/blfs-bootscripts-$RELEASE.tar blfs-bootscripts-$RELEASE/ &&
+cd $ORPWD &&
+bzip2 -9 $ORPWD/blfs-bootscripts-$RELEASE.tar &&
+echo "blfs-bootscripts-$RELEASE.tar.bz2 created successfully"
+
+fi
+# End
