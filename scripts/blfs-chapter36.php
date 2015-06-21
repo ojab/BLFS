43ba742a499d29a54b3216f70da301deca3e7eb6
bdubbs <bdubbs@af4574ff-66df-0310-9fd7-8a98e5e911e0>
2015-06-21 06:40:06 +0000

    Fix rxvt-unicode for upstream change
    git-svn-id: svn://svn.linuxfromscratch.org/BLFS/trunk@16144 af4574ff-66df-0310-9fd7-8a98e5e911e0

--- scripts/blfs-chapter36.php
+++ scripts/blfs-chapter36.php
@@ -253,7 +253,7 @@ function get_packages( $package, $dirpath )
 
   // Most packages are in the form $package-n.n.n
   // Occasionally there are dashes (e.g. 201-1)
-  $max = find_max( $lines, "/$package/", "/^.*$package-([\d\.]*\d)\.tar.*$/" );
+  $max = find_max( $lines, "/$package/", "/^.*$package-([\d\.]*\d)\.ta.*$/" );
   return $max;
 }
 
