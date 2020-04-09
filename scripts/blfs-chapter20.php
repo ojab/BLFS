8b65fe775a9aa05e9e9f16394b262f200d741b87
bdubbs <bdubbs@af4574ff-66df-0310-9fd7-8a98e5e911e0>
2020-04-09 05:30:03 +0000

    Prefer ipv4 when fetching most data
    git-svn-id: svn://svn.linuxfromscratch.org/BLFS/trunk@22976 af4574ff-66df-0310-9fd7-8a98e5e911e0

--- scripts/blfs-chapter20.php
+++ scripts/blfs-chapter20.php
@@ -151,7 +151,7 @@ function get_packages( $package, $dirpath )
      }
 
      $lines = http_get_file( $dirpath );
-print_r($lines);
+//print_r($lines);
      if ( ! is_array( $lines ) ) return $lines;
   } // End fetch
 
--- scripts/blfs-include.php
+++ scripts/blfs-include.php
@@ -160,7 +160,7 @@ function http_get_file( $url )
          preg_match( "/docutils/", $url    ) ||
          preg_match( "/expect/", $url      ) ) 
   {
-     exec( "curl -L -s -m40 -A Firefox/41.0 $url", $dir );
+     exec( "curl -4 -L -s -m40 -A Firefox/41.0 $url", $dir );
      $s   = implode( "\n", $dir );
      $dir = strip_tags( $s );
      $strip =  explode( "\n", $dir );
